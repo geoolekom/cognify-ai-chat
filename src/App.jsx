afd11ebeab1830e36b0bbeb7e8f38e60907c7403
Yehor Komarov <ekomarov@constructor.tech>
2026-03-13 19:15:22 +0400

    chat panel refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-import MessageList from './ChatPanel/MessageList';
-import MessageForm from './ChatPanel/MessageForm';
 import Sidebar from './Sidebar/Sidebar';
+import ChatPanel from './ChatPanel/ChatPanel';
 
 function App() {
     const initialMessages = [
         { id: 1, role: 'user', text: 'Hello, how are you?' },
         { id: 2, role: 'assistant', text: "I'm doing great, thanks for asking!" },
         { id: 3, role: 'user', text: "That's wonderful to hear!" },
+        { id: 4, role: 'assistant', text: 'Thanks!' },
     ];
     const [messages, setMessages] = useState(initialMessages);
 
@@ -21,10 +21,7 @@ function App() {
     return (
         <>
             <Sidebar activeConversationID={activeConversationID} setActiveConversationID={setActiveConversationID} />
-            <main className="flex flex-col flex-1">
-                <MessageList messages={messages} />
-                <MessageForm appendMessage={appendMessage} />
-            </main>
+            <ChatPanel messages={messages} appendMessage={appendMessage} />
         </>
     );
 }
