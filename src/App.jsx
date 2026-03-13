b1eefbdc01838d7b467cb57d2bf3134e47491912
Yehor Komarov <ekomarov@constructor.tech>
2026-03-13 19:54:16 +0400

    prototype using mock API

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect, act } from 'react';
 import './App.css';
 import Sidebar from './Sidebar/Sidebar';
 import ChatPanel from './ChatPanel/ChatPanel';
+import { getMessages, createMessage } from './api/messages';
 
 function App() {
-    const initialMessages = [
-        { id: 1, role: 'user', text: 'Hello, how are you?' },
-        { id: 2, role: 'assistant', text: "I'm doing great, thanks for asking!" },
-        { id: 3, role: 'user', text: "That's wonderful to hear!" },
-        { id: 4, role: 'assistant', text: 'Thanks!' },
-    ];
-    const [messages, setMessages] = useState(initialMessages);
+    const [messages, setMessages] = useState([]);
+    const [activeConversationID, setActiveConversationID] = useState(1);
+
+    useEffect(() => {
+        getMessages(activeConversationID).then(setMessages);
+    }, [activeConversationID, messages]);
 
     function appendMessage(input) {
-        setMessages([...messages, { id: messages.length + 1, role: 'user', text: input }]);
+        createMessage(activeConversationID, 'user', input).then(() => {
+            setMessages([...messages]);
+        });
     }
-
-    const [activeConversationID, setActiveConversationID] = useState(1);
 
     return (
         <>
--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,40 +1,31 @@
-import { useState } from 'react';
-import Conversation from './Conversation';
+import { useState, useEffect } from 'react';
+import ConversationList from './ConversationList';
+import AddButton from './AddButton';
+import { createConversation, getConversations } from '../api/conversations';
 
 function Sidebar({ activeConversationID, setActiveConversationID }) {
-    const initialConversations = [
-        { id: 1, title: 'How is your day?' },
-        { id: 2, title: 'Help me with homework' },
-    ];
-    const [conversations, setConversations] = useState(initialConversations);
-    const conversationElements = [];
-    for (const conversation of conversations) {
-        conversationElements.push(
-            <Conversation
-                key={conversation.id}
-                title={conversation.title}
-                isActive={activeConversationID === conversation.id}
-                setActive={() => setActiveConversationID(conversation.id)}
-            />,
-        );
-    }
+    const [conversations, setConversations] = useState([]);
 
-    console.log(conversationElements);
+    useEffect(() => {
+        getConversations().then(setConversations);
+    }, [conversations]);
 
     function createNewConversation() {
-        const newConversation = { id: conversations.length + 1, title: 'New Conversation' };
-        setConversations([...conversations, newConversation]);
-        setActiveConversationID(newConversation.id);
+        createConversation('New Conversation').then((newConversation) => {
+            setActiveConversationID(newConversation.id);
+        });
     }
 
     return (
         <aside className="flex flex-col w-64 bg-gray-900 text-white p-4">
             <div>
-                <button className="w-full bg-red-500 hover:bg-red-600 p-3 my-1" onClick={createNewConversation}>
-                    + New Chat
-                </button>
+                <AddButton createNewConversation={createNewConversation} />
             </div>
-            {conversationElements}
+            <ConversationList
+                conversations={conversations}
+                activeConversationID={activeConversationID}
+                setActiveConversationID={setActiveConversationID}
+            />
         </aside>
     );
 }
