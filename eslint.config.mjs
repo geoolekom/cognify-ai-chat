624f9a8a92f3a4c7f8ce15d3280e496eefcc497c
Yehor Komarov <ekomarov@constructor.tech>
2026-03-20 19:09:19 +0400

    base nextjs app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
-export default defineConfig([
-    {
-        files: ['**/*.{js,mjs,cjs}'],
-        plugins: { js },
-        extends: ['js/recommended'],
-        languageOptions: { globals: globals.browser },
-    },
-    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'src-plain-js/**',
+        'next-env.d.ts',
+    ]),
 ]);
+
+export default eslintConfig;
--- src/ChatPanel/ChatPanel.jsx
+++ src/ChatPanel/ChatPanel.jsx
@@ -1,7 +1,22 @@
+'use client';
 import MessageList from './MessageList';
 import MessageForm from './MessageForm';
+import { useState, useEffect } from 'react';
+import { getMessages, createMessage } from '../api/messages';
 
-function ChatPanel({ messages, appendMessage }) {
+function ChatPanel({ activeConversationID }) {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        getMessages(activeConversationID).then(setMessages);
+    }, [messages, activeConversationID]);
+
+    function appendMessage(input) {
+        createMessage(activeConversationID, input).then(() => {
+            setMessages([...messages]);
+        });
+    }
+
     return (
         <main className="flex flex-col flex-1">
             <MessageList messages={messages} />
