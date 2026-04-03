58cc11cea15ee1b0ba5dace1a7b7f1b56483a58a
Yehor Komarov <ekomarov@constructor.tech>
2026-04-03 18:09:19 +0400

    useRouter instead of rediect and vercel ai package installed

--- src/hooks/conversations.js
+++ src/hooks/conversations.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export function useConversationsQuery() {
     return useQuery({
@@ -9,13 +9,15 @@ export function useConversationsQuery() {
 }
 
 export function useConversationsMutation(title) {
+    const router = useRouter();
     const body = JSON.stringify({ title });
     const queryClient = useQueryClient();
     return useMutation({
         mutationFn: () => fetch('/api/conversations', { method: 'POST', body }).then((r) => r.json()),
         onSuccess: (r) => {
             queryClient.invalidateQueries({ queryKey: ['conversations'] }).then(() => {
-                redirect(`/chats/${r.id}`);
+                router.push(`/chats/${r.id}`);
+                // redirect(`/chats/${r.id}`);
             });
         },
     });
