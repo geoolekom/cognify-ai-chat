a7b9feeb262e080428181921c089b3005e3c0e56
Yehor Komarov <ekomarov@constructor.tech>
2026-04-20 17:14:57 +0400

    service worker and offline page

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'cognify-v1';
+const SHELL = ['/', '/manifest.webmanifest', '/icon-640.png', '/offline.html'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))),
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    const { request } = event;
+    if (request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        fetch(request)
+            .then((res) => {
+                const copy = res.clone();
+                caches.open(CACHE).then((c) => c.put(request, copy));
+                return res;
+            })
+            .catch(() => caches.match(request)),
+    );
+});
